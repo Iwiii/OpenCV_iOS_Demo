7db02af3c16693b64a6e1ac3ec43df5e9007134c
于玮 <ywcoone@163.com>
2018-01-15 15:19:41 +0800

    新建iOS 工程

---
+++ OpenCVDemo/OpenCVDemoTests/OpenCVDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  OpenCVDemoTests.m
+//  OpenCVDemoTests
+//
+//  Created by 于玮 on 2018/1/15.
+//  Copyright © 2018年 于玮. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OpenCVDemoTests : XCTestCase
+
+@end
+
+@implementation OpenCVDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
